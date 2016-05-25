204e64ec2e033bf9e27f7e54574271c8348619b2
ilenia arangiaro <ilenia arangiaro>
2016-05-25 15:26:23 +0200

    first version

---
+++ public/index.php
@@ -0,0 +1,165 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Meteo</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+        <link href='https://fonts.googleapis.com/css?family=Quicksand:400,700' rel='stylesheet' type='text/css'>
+
+        <!-- font-awesome -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/slick.css">
+        <link rel="stylesheet" href="css/slick-theme.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/weather-icons-wind.min.css">
+        <link rel="stylesheet" href="css/weather-icons.min.css">
+
+
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <div id="app" class="container-fluid">
+
+          <!-- loading -->
+          <section id="loading">
+            <div id="loading-image"></div>
+            <div class="row">
+              <div class="col-sm-12">
+                <div id="loading-message"></div>
+
+              </div>
+            </div>
+          </section>
+
+
+          <!-- home -->
+          <section id="home">
+
+
+            <div class="row">
+              <div class="col-sm-12 centered-holder">
+
+                <div class="slideshow">
+                  <!-- WEATHER -->
+                  <div id="weather">
+                    <ul>
+                      <div class="icon"><i></i></div>
+                      
+                      <li><div class="string"></div></li>
+                      <li><div class="humidity"></div></li>
+                      <li><div class="wind"></div></li>
+                      <li><div class="temp">
+                        <div class="high"></div>
+                        <div class="low"></div>
+                      </div></li>
+                    </ul>
+                    <!--
+                    <ul id="forecast">
+                    <ul> -->
+                  </div>
+
+
+                  <!-- WEAR -->
+                  <div id="wear">
+                    <ul id="accessories">
+                      <li class="option1">glasses</li>
+                      <li class="option2">umbrella</li>
+                      <li class="option3">cap</li>
+                    </ul>
+                    <ul id="top">
+                      <li class="option1">t-shisrt</li>
+                      <li class="option2">jacket</li>
+                      <li class="option3">light jacket</li>
+                      <li class="option4">k-way</li>
+                    </ul>
+                    <ul id="backpack">
+                      <li class="option1">water-proof</li>
+                      <li class="option2">breathable</li>
+                    </ul>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <!-- info -->
+          <section id="info">
+            <div class="row">
+              <div class="col-sm-12 centered-holder">
+                <div class="centered">
+                  <p>This is the info section</p>
+                </div>
+              </div>
+            </div>
+          </section>
+
+
+          <footer>
+            <a href="./" class="link-home"><i class="fa fa-home"></i></a>
+            <a href="#" class="link-refresh"><i class="fa fa-refresh"></i></a>
+            <a href="#" class="link-info"><i class="fa fa-info"></i></a>
+          </footer>
+        </div>
+
+
+        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+        <script src="js/purl.js"></script>
+        <script src="js/plugins.js"></script>
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+        <script src="js/shake.js"></script>
+        <script src="js/jquery.simpleWeather.js"></script>
+        <script src="js/slick.min.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBIRqmuU3qE-9IQ3KbTENdnCNXKzI0rqYQ"
+        async defer></script>
+
+        <!-- SCRIPT INIT -->
+        <script>
+
+          $(document).ready(function(){
+            init()
+            showSection("loading")
+            getPosition()
+
+          })
+        </script>
+    </body>
+</html>
---
+++ public/js/main.js
@@ -0,0 +1,378 @@
+var debug = true
+
+
+function init(){
+  document.body.addEventListener('touchmove', function(event) {
+    event.preventDefault();
+  });
+
+  $(".link-refresh").on("click",function(){
+    getPosition()
+  })
+
+
+  // on resize
+  centerElements()
+  $(window).resize(function() {
+    centerElements()
+  });
+
+  // SHAKE EVENT
+  var myShakeEvent = new Shake({
+      threshold: 15, // optional shake strength threshold
+      timeout: 1000 // optional, determines the frequency of event generation
+  });
+  myShakeEvent.start();
+  window.addEventListener('shake', reactToShakeEvent, false);
+}
+
+
+//function to call when shake occurs
+function reactToShakeEvent () {
+  getPosition()
+}
+
+
+function showSection(section_id) {
+  $("#app section").hide()
+  $("#app section#"+section_id).show()
+
+  $("body").attr("class","section-"+section_id)
+  centerElements()
+}
+
+
+
+function info() {
+  showSection("info")
+}
+
+function getPosition() {
+  showSection("loading")
+  showLoadingMessage("getting position...")
+  if(debug){ // if debug use fake location
+    getWeather(45.559394399999995,10.2037211)
+  } else {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      getWeather(position.coords.latitude,position.coords.longitude)
+    });
+  }
+}
+
+
+function getWeather(latitude,longitude) {
+  showLoadingMessage("getting weather...")
+  $.simpleWeather({
+    location: latitude+","+longitude,
+    woeid: '',
+    unit: 'c',
+    success: function(weather) {
+      renderWeather(weather, latitude, longitude)
+    },
+    error: function(error) {
+      alert("error: "+error)
+    }
+  });
+
+}
+
+
+function renderWeather(weather, latitude, longitude) {
+  //console.log(weather)
+
+  // SLIDESHOW
+
+  $('.slideshow').slick({
+    //setting-name: setting-value
+     dots: true
+  });
+
+  var my_code = getOurWeatherCode(weather.code)
+  var my_string = getOurWeatherString(my_code)
+  //alert(my_string)
+  $("#weather .image").addClass("weather-"+my_code)
+  $("#weather .string").text(my_string)
+  $("#weather .humidity").text(weather.humidity+"%")
+  $("#weather .wind").text(weather.wind.speed+" m/s, "+weather.wind.direction)
+  $("#weather .temp .high").text(weather.high)
+  $("#weather .temp .low").text(weather.low)
+
+  var icon_class = getOurWeatherIconClass(my_code);
+  $("#weather .icon i").addClass(icon_class)
+
+  showSection("home")
+
+  // forecast
+  $("#forecast").html("") // empty #forecast ul
+  for(var i in weather.forecast) {
+    var forecast = weather.forecast[i]
+    var forecast_my_code = getOurWeatherCode(forecast.code)
+    var forecast_my_string = getOurWeatherString(my_code)
+
+    var html_forecast ='<li class="weather-'+forecast_my_code+'">'
+    html_forecast += '<div>'+forecast_my_string+'</div>'
+    html_forecast += '<div class="temp"><div class="high">'+forecast.high+'</div><div class="low">'+forecast.low+'</div></div>'
+    html_forecast += '</li>'
+
+    $("#forecast").prepend(html_forecast)
+    //console.log(forecast)
+  }
+
+  var suggestions = {
+    "1": {
+      "accessories": 1,
+      "top": 1,
+      "backpack": 2
+    },
+    "2": {
+      "accessories": 1,
+      "top": 3,
+      "backpack": 2
+    },
+    "3": {
+      "accessories": 1,
+      "top": 3,
+      "backpack": 2
+    },
+    "4": {
+      "accessories": 2,
+      "top": 2,
+      "backpack": 1
+    },
+    "5": {
+      "accessories": 2,
+      "top": 2,
+      "backpack": 1
+    },
+    "6": {
+      "accessories": 3,
+      "top": 2,
+      "backpack": 1
+    },
+    "7": {
+      "accessories": 3,
+      "top": 3,
+      "backpack": 2
+    },
+    "8": {
+      "accessories": 2,
+      "top": 2,
+      "backpack": 1
+    },
+    "9": {
+      "accessories": 3,
+      "top": 2,
+      "backpack": 1
+    },
+    "10": {
+      "accessories": 2,
+      "top": 2,
+      "backpack": 1
+    },
+    "11": {
+      "accessories": 2,
+      "top": 4,
+      "backpack": 1
+    }
+  }
+
+  var suggestion = suggestions[my_code]
+  // adjust base in temperature
+  var avg_temp = (parseInt(weather.high)+parseInt(weather.low))/2
+  if(avg_temp > -50) suggestion.top = 2
+  if(avg_temp > 10) suggestion.top = 3
+  if(avg_temp > 20) suggestion.top = 1
+  if(my_code == "8" || my_code == "10" || my_code == "11") suggestion.top = 4
+
+  $("#wear ul li").hide()
+  $("#wear #accessories .option"+suggestion.accessories).show()
+  $("#wear #top .option"+suggestion.top).show()
+  $("#wear #backpack .option"+suggestion.backpack).show()
+
+
+  /*
+  var geocoder = new GClientGeocoder();
+  var point = new GLatLng(37.4419, -122.1419)
+  geocoder.getLocations(point, function(addresses) {
+    if(addresses.Status.code != 200) {
+      alert("reverse geocoder failed to find an address for " + latlng.toUrlValue());
+    }
+    else {
+      address = addresses.Placemark[0];
+      console.log(addresses)
+    }
+  });
+  */
+  /*
+  var map;
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: latitude, lng: longitude},
+    zoom: 15
+  });
+  */
+
+}
+
+
+function showLoadingMessage(message){
+  $("#loading-message").text(message)
+}
+
+
+function getOurWeatherCode(its_code){
+  /*
+  1	sunny
+  2	clear day
+  3	clear night
+  4	cloudy day
+  5	cloudy night
+  6	foggy
+  7	windy
+  8	rainy
+  9	cold
+  10	snow
+  11	tunderstorm
+  */
+
+  var our_codes = {
+  "0": "11",
+  "1": "11",
+  "2": "11",
+  "3": "11",
+  "4": "11",
+  "5": "10",
+  "6": "8",
+  "7": "10",
+  "8": "8",
+  "9": "8",
+  "10": "8",
+  "11": "8",
+  "12": "8",
+  "13": "10",
+  "14": "10",
+  "15": "10",
+  "16": "10",
+  "17": "11",
+  "18": "10",
+  "19": "7",
+  "20": "6",
+  "21": "6",
+  "22": "6",
+  "23": "7",
+  "24": "7",
+  "25": "9",
+  "26": "4",
+  "27": "5",
+  "28": "4",
+  "29": "5",
+  "30": "4",
+  "31": "3",
+  "32": "1",
+  "33": "3",
+  "34": "2",
+  "35": "8",
+  "36": "1",
+  "37": "11",
+  "38": "11",
+  "39": "11",
+  "40": "8",
+  "41": "10",
+  "42": "10",
+  "43": "10",
+  "44": "4",
+  "45": "11",
+  "46": "10",
+  "47": "10",
+  "3200": "0"
+  }
+  return our_codes[its_code]
+}
+
+function getOurWeatherString(our_code) {
+    var our_strings = {
+      "1": "sunny",
+      "2": "clear day",
+      "3": "clear night",
+      "4": "cloudy day",
+      "5": "cloudy night",
+      "6": "foggy",
+      "7": "windy",
+      "8": "rainy",
+      "9": "cold",
+      "10": "snow",
+      "11": "tunderstorm"
+    }
+    return our_strings[our_code]
+}
+
+
+function centerElements(){
+  $(".centered-holder").each(function(index, element){
+    var x =  $(this).width()/2-$(this).find(".centered").width()/2
+    var y =  $(this).height()/2-$(this).find(".centered").height()/2
+    if(y<0) y=0
+    $(this).find(".centered").css({
+      left: x+"px",
+      top: y+"px"
+    })
+  })
+}
+
+
+//assegnare immagine ai codici
+//var cloud = '<i class="wi wi-day-cloudy"></i>'
+// cos'è questa variabile sopra? a cosa servirebbe?
+
+function getOurWeatherIconClass(our_code) { // qui mancava our_code come paramtro in ingresso
+    var our_classes = {
+      "1": "wi-day-sunny",
+      "2": "wi-day-sunny",
+      "3": "wi-day-sunny",
+      "4": "wi-day-sunny",
+      "5": "wi-day-sunny",
+      "6": "wi-day-sunny",
+      "7": "wi-day-sunny",
+      "8": "wi-day-sunny",
+      "9": "wi-day-sunny",
+      "10": "wi-day-sunny",
+      "11": "wi-day-sunny"
+    }
+    var our_class = "wi "+our_classes[our_code]
+    alert(our_class)
+    return our_class
+}
+
+/*
+NOTE MOLINARI
+$('.image').getImage()
+non puoi chiamare la tua funzione GetImage direttamente su jQuery
+non sapete ancora "aggiungere" funzioni o comandi direttamente su
+jquery, quindi la devi chiamare, semplicemente con
+getImage()
+
+ho rinominato la funzione da getImage a getOurWeatherIconClass
+ho lasciato a tutti i codici "wi-day-sunny" sistemalo tu con i nomi corretti leggendo qui
+https://erikflowers.github.io/weather-icons/
+
+altra cosa, la devi chiamare nel momento/punto giusto, la
+metto dentro la funzione renderWeather(), cercala qui sopra
+
+in index.php hai incluso due volte i due css, sia .min che normali
+non serve; tra l'altro hai scritto weather-icons.mins.css
+mentre invece è weather-icons.min.css
+quindi le icone non le avresti mai viste :-)
+
+hai incluso erroneamente i fonts delle weather icons in /public/css/fonts
+per nostra sfortuna (non piace neanche a me come scelta) i fonts
+ nel css di weather icons se li aspetta in ../font, quindi li
+ho spostati in /public/font
+
+ps: mi permetto di fare minuscola l'iniziale, se vuoi sapere perchè
+alla prossima lezione ve lo spiego :-)
+
+ho messo questo nel .css
+#weather .icon i {
+  font-size: 4em;
+}
+
+*/
